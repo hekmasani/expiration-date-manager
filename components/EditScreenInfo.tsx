9464a1c47948efa9c5e61b15d4b55b89aa9f7b9c
hekma <contact@hekma.dev>
2026-05-19 21:59:48 +0200

    refactor: linting

--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 
+import Colors from '@/constants/Colors';
+
 import { ExternalLink } from './ExternalLink';
 import { MonoText } from './StyledText';
 import { Text, View } from './Themed';
 
-import Colors from '@/constants/Colors';
 
 export default function EditScreenInfo({ path }: { path: string }) {
   return (
@@ -38,7 +39,7 @@ export default function EditScreenInfo({ path }: { path: string }) {
           style={styles.helpLink}
           href="https://docs.expo.io/get-started/create-a-new-app/#opening-the-app-on-your-phonetablet">
           <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
-            Tap here if your app doesn't automatically update after making changes
+            {"Tap here if your app doesn't automatically update after making changes"}
           </Text>
         </ExternalLink>
       </View>
